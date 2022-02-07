a007d1dea7ce8c7c9f1e3f105e95a6cb50edd761
markendroid <markkurtukov@gmail.com>
2022-02-08 00:39:41 +0700

    add x2 experimental

--- LAB_0/main.c
+++ LAB_0/main.c
@@ -1,7 +1,31 @@
 #include <stdio.h>
+#include <math.h>
 #include <stdlib.h>
+#include <string.h>
 #include <time.h>
 
+#define INTERVAL 100	// Точность после запятой
+#define N 1000000		// Кол-во сгенерированных чисел
+
+
+float x2(int counting[]){
+
+	float x2_result;	//эксперементальное x^2
+	float p_i;			//𝑝𝑖 – теоретическая вероятность попадания 
+						//чисел в i-ый интервал равна 𝑝𝑖=1/𝑘
+	x2_result = 0;
+	p_i = 1.0 / (INTERVAL + 1);
+
+	for (int i = 0; i <= INTERVAL; i++){
+		x2_result += (pow(counting[i], 2) / p_i);
+	}
+
+	//printf("%.f\n", x2_result);
+	x2_result = (x2_result / N) - N;
+
+	return x2_result;
+
+}
 
 int main(){
 
@@ -11,14 +35,14 @@ int main(){
 
 	int counting[101];
 
-	for (int i = 0; i < 101; i++){
+	for (int i = 0; i <= INTERVAL; i++){
 		counting[i] = 0;
 	}
 
 	printf("Randomizing ....\n");
 
-	for (int i = 0; i < 1000000; i++){
-		int x = rand() % 101;
+	for (int i = 0; i <= N; i++){
+		int x = rand() % (INTERVAL + 1);
 		counting[x]++;
 	}
 
@@ -26,13 +50,17 @@ int main(){
 	fprintf (result, "Num Count\n");
 	printf("Writing result ....\n");
 
-	for (int i = 0; i <= 100; i++){
-		float k = (float)i / 100;
+	for (int i = 0; i < INTERVAL; i++){
+		float k = (float)i / INTERVAL;
 		//printf("%.2f %d\n", k, counting[i]);
-
 		fprintf (result, "%.2f %d\n", k, counting[i]);
 	}
 	printf("Ready!\n");
 	fclose(result);
+
+	float res_x2 = x2(counting);
+
+	printf ("%f \n", res_x2);
+
 	return 0;
 }
